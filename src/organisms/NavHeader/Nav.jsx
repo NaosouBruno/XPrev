1e4e4bd5148fb2e58c3f01b251a8ec9aca3c1173
Bruno Silva <brunoowap@gmail.com>
2023-11-18 13:02:52 -0400

    estilizando nav header no mobile

--- src/organisms/NavHeader/Nav.jsx
+++ src/organisms/NavHeader/Nav.jsx
@@ -10,9 +10,11 @@ export default function NavHeader() {
     <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
       <div className="nav-group">
         <BurguerMenu onToggle={toggleMenu} />
-        <h2>X-Prev</h2>
+        <div className="nav-containerTitle">
+          <h2 className="nav-text nav-text--title">X-Prev</h2>
+        </div>
       </div>
-      <ul className="nav-items">
+      <ul className="nav-items nav-text">
         <li key="item1">Quem Somos</li>
         <li key="item2">Serviços</li>
         <li key="item3">Agende sua consulta</li>
