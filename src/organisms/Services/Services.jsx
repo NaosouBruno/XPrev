051e8634dfe4f1647ecc297221305d8550663bc3
Bruno Silva <brunoowap@gmail.com>
2023-12-09 16:16:13 -0400

    Iniciando componente de serviços

---
+++ src/organisms/Services/Services.jsx
@@ -0,0 +1,17 @@
+import "./services.scss";
+function Services (){
+    return(
+        <section className="containerServices">
+            <h2>Conheças nossos serviços</h2>
+            <ul>
+                <li>Aposentadoria</li>
+                <li>Revisão</li>
+                <li>Planejamento</li>
+                <li>Pensão e auxilios</li>
+            </ul>
+            <button>Entrar em contato</button>
+        </section>
+    )
+}
+
+export default Services
