e9dd5f6fa4b7bbb8e823d458d64a799a432f812d
Bruno Silva <brunoowap@gmail.com>
2023-12-28 15:04:59 -0400

    Iniciando seção sobre a empresa

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import ContactForm from "./organisms/ContactForm/ContactForm";
 import NavHeader from "./organisms/NavHeader/Nav";
 import Intro from "./organisms/Intro/Intro";
 import Services from "./organisms/Services/Services";
+import About from "./organisms/About/about";
 
 function App() {
   return (
     <>
       <NavHeader />
       <Intro />
+      <About />
       <Services />
       <ContactForm />
     </>
---
+++ src/organisms/About/About.jsx
@@ -0,0 +1,23 @@
+import "./about.scss"
+function About() {
+  return (
+    <section className="containerAbout">
+      <h2>Sobre a XPrev</h2>
+      <div>
+        <p>
+          A XPrev é uma equipe especializada em Assessoria Administrativa.
+          Orientamos e solucionamos questões relacionadas ao Regime Geral (INSS)
+          e Regime Próprio de Previdência (Servudires Públicos), em âmbito
+          Administrativo.
+        </p>
+        <p>
+          Na XPrev o cliente recebe atendimento personalizado e pode, assim,
+          receber seus benefícios que tiver direito da forma mais ágil e
+          eficiente possível.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About
